28c600c89f8d284a583481933ce38bc9efa5f75a
Joecheong2006 <joecheong2006@gmail.com>
2025-07-10 14:38:43 -0700

    Add TBO

---
+++ src/glUtilities/TextureBuffer.cpp
@@ -0,0 +1,45 @@
+#include "glUtilities/TextureBuffer.h"
+#include "glad/glad.h"
+#include "util.h"
+
+namespace gl {
+    TextureBuffer::TextureBuffer(const void *data, i32 size, i32 usage, i32 internalFormat)
+        : m_size(size), m_usage(usage), m_internalFormat(internalFormat)
+    {
+        GLCALL(glGenBuffers(1, &m_tbo));
+        GLCALL(glBindBuffer(GL_TEXTURE_BUFFER, m_tbo));
+        GLCALL(glBufferData(GL_TEXTURE_BUFFER, size, data, usage));
+
+        GLCALL(glGenTextures(1, &m_tboTexture));
+        GLCALL(glBindTexture(GL_TEXTURE_BUFFER, m_tboTexture));
+        GLCALL(glTexBuffer(GL_TEXTURE_BUFFER, internalFormat, m_tbo););
+    }
+
+    TextureBuffer::~TextureBuffer() {
+        GLCALL(glGenBuffers(1, &m_tbo));
+        GLCALL(glGenTextures(1, &m_tboTexture));
+    }
+
+    void TextureBuffer::setBuffer(const void *data, i32 size) {
+        GLCALL(glBindBuffer(GL_TEXTURE_BUFFER, m_tbo));
+        GLCALL(glBufferData(GL_TEXTURE_BUFFER, size, data, m_usage));
+        m_size = size;
+    }
+
+    void TextureBuffer::updateBuffer(const void *data, i32 offset, i32 size) {
+        GLCALL(glBindBuffer(GL_TEXTURE_BUFFER, m_tbo));
+        GLCALL(glBufferSubData(GL_TEXTURE_BUFFER, offset, size, data));
+        m_size = size;
+    }
+
+    void TextureBuffer::bind(u32 slot) const {
+        GLCALL(glActiveTexture(GL_TEXTURE0 + slot));
+        GLCALL(glBindTexture(GL_TEXTURE_BUFFER, m_tboTexture));
+    }
+
+    void TextureBuffer::unbind() const {
+        GLCALL(glBindBuffer(GL_TEXTURE_BUFFER, 0));
+        GLCALL(glBindTexture(GL_TEXTURE_BUFFER, 0));
+    }
+
+}
---
+++ src/glUtilities/TextureBuffer.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../util.h"
+
+namespace gl {
+    class TextureBuffer {
+    private:
+        u32 m_tbo, m_tboTexture;
+        i32 m_size, m_usage, m_internalFormat;
+
+    public:
+        TextureBuffer(const void *data, i32 size, i32 usage, i32 internalFormat);
+        ~TextureBuffer();
+
+        void setBuffer(const void *data, i32 size);
+        void updateBuffer(const void *data, i32 offset, i32 size);
+
+        void bind(u32 slot) const;
+        void unbind() const;
+
+        i32 getSize() const { return m_size; }
+        i32 getInternalFormat() const { return m_internalFormat; }
+
+    };
+
+}
