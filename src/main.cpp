27059b35849288afb503733dc17968bd48ae3f01
LVOrac <joecheong2006@gmail.com>
2025-07-10 20:39:24 -0700

    Fixed DPI scaling in Windows

--- src/main.cpp
+++ src/main.cpp
@@ -109,6 +109,8 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GL_TRUE);
+
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
