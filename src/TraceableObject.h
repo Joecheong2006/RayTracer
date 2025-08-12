bec282486294db8f24610c9c3b2a5488b6d26240
LVOrac <joecheong2006@gmail.com>
2025-08-12 12:59:23 -0700

    Fixed AABB error boundary

--- src/TraceableObject.h
+++ src/TraceableObject.h
@@ -21,13 +21,13 @@ struct AABB {
     {}
 
     AABB(const AABB &box1, const AABB &box2) {
-        min.x = std::min(box1.min.x, box2.min.x) - 1e3;
-        min.y = std::min(box1.min.y, box2.min.y) - 1e3;
-        min.z = std::min(box1.min.z, box2.min.z) - 1e3;
+        min.x = std::min(box1.min.x, box2.min.x) - 1e-5;
+        min.y = std::min(box1.min.y, box2.min.y) - 1e-5;
+        min.z = std::min(box1.min.z, box2.min.z) - 1e-5;
 
-        max.x = std::max(box1.max.x, box2.max.x) + 1e3; 
-        max.y = std::max(box1.max.y, box2.max.y) + 1e3; 
-        max.z = std::max(box1.max.z, box2.max.z) + 1e3; 
+        max.x = std::max(box1.max.x, box2.max.x) + 1e-5;
+        max.y = std::max(box1.max.y, box2.max.y) + 1e-5;
+        max.z = std::max(box1.max.z, box2.max.z) + 1e-5;
     }
 
     glm::vec3 min, max;
